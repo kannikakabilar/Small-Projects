30c38475e2ca0b5795a2d0cd04d8a4a5288e535a
Kannika Kabilar <57203769+kannikakabilar@users.noreply.github.com>
2023-02-07 15:15:52 -0500

    Add files via upload

---
+++ kan-react-app/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+const myFirstElement = <h1>Hello React! KK</h1>
+const myElement = <p>new line kannika</p>
+
+//const root = ReactDOM.createRoot(document.getElementById('root'));
+//root.render(myElement);
+//ReactDOM.render(myElement, document.getElementById('root'));
+// https://www.youtube.com/watch?v=uiW4tArFLkE
+// https://www.youtube.com/watch?v=2FeymQoKvrk
+ReactDOM.createRoot(document.getElementById('root')).render(myFirstElement);
